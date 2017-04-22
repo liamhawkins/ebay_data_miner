7366155eb01749d745c5e07bf4641d4a0de998c9
Liam Hawkins <liamhawkinscu@gmail.com>
2017-04-22 12:10:05 -0400

    Refactor method/variable names for clarity

--- miner.py
+++ miner.py
@@ -10,59 +10,61 @@ from collections import OrderedDict
 from prompt_toolkit import prompt
 
 
-ATTRIBUTES = OrderedDict()
-ATTRIBUTES['model'] = 'Model'
-ATTRIBUTES['processor'] = 'Processor'
-ATTRIBUTES['ram_cap'] = 'Ram Capacity'
-ATTRIBUTES['pri_hd_cap'] = 'Primary HD Capacity'
-ATTRIBUTES['pri_hd_type'] = 'Primary HD Type'
-ATTRIBUTES['sec_hd_cap'] = 'Secondary HD Capacity'
-ATTRIBUTES['sec_hd_type'] = 'Secondary HD Type'
-ATTRIBUTES['os'] = 'OS'
-ATTRIBUTES['battery'] = 'Battery Included'
-ATTRIBUTES['ac_charger'] = 'AC Charger Included'
+MANUAL_ATTRIBUTES = OrderedDict()
+MANUAL_ATTRIBUTES['model'] = 'Model'
+MANUAL_ATTRIBUTES['processor'] = 'Processor'
+MANUAL_ATTRIBUTES['ram_cap'] = 'Ram Capacity'
+MANUAL_ATTRIBUTES['pri_hd_cap'] = 'Primary HD Capacity'
+MANUAL_ATTRIBUTES['pri_hd_type'] = 'Primary HD Type'
+MANUAL_ATTRIBUTES['sec_hd_cap'] = 'Secondary HD Capacity'
+MANUAL_ATTRIBUTES['sec_hd_type'] = 'Secondary HD Type'
+MANUAL_ATTRIBUTES['os'] = 'OS'
+MANUAL_ATTRIBUTES['battery'] = 'Battery Included'
+MANUAL_ATTRIBUTES['ac_charger'] = 'AC Charger Included'
 
-SCRAPES = ['ebay_id', 'date_complete', 'sold',
-           'listing_type', 'country', 'top_rated',
-           'price', 'shipping']
+AUTO_SCRAPE_ATTRIBUTES = ['ebay_id', 'date_complete', 'sold',
+                          'listing_type', 'country', 'top_rated',
+                          'price', 'shipping']
+
 
 class DataInput:
 
-    def __init__(self, attributes, scrapes):
-        self.attributes = attributes
-        self.scrapes = scrapes
-        self.manual_input_df = pd.DataFrame(columns=self.attributes.keys())
-        self.scrapes_df = pd.DataFrame(columns=self.scrapes)
+    def __init__(self, manual_attributes, auto_scrape_attributes):
+        self.manual_attributes = manual_attributes
+        self.auto_scrape_attributes = auto_scrape_attributes
+        self.manual_attribute_df = pd.DataFrame(columns=self.manual_attributes.keys())
+        self.auto_scrape_attribute_df = pd.DataFrame(columns=self.auto_scrape_attributes)
         self.full_attribute_df = pd.DataFrame()
 
-    def print_attributes(self):
-        for attrib, question in self.attributes.items():
+    def print_manual_attributes(self):
+        for attrib, question in self.manual_attributes.items():
             print('Attribute: {} - Question: {}:'.format(attrib, question))
 
-    def print_data(self):
-        print(self.manual_input_df)
+    def print_auto_scrap_attributes(self):
+        for attrib in self.auto_scrape_attributes:
+            print(attrib)
 
     def get_manual_input(self, ebay_ids):
         for item in ebay_ids:
             inp_list = []
-            for attrib, question in self.attributes.items():
+            for attrib, question in self.manual_attributes.items():
                 inp_list.append(prompt('{} - {}: '.format(item, question)))
-            self.manual_input_df.loc[len(self.manual_input_df.index) + 1] = inp_list
+            self.manual_attribute_df.loc[len(self.manual_attribute_df.index) + 1] = inp_list
 
-    def scrape_input(self, ebay_ids):
+    def scrape_attributes(self, ebay_ids):
         for item in ebay_ids:
             scrape_list = []
-            for attrib in self.scrapes:
+            for attrib in self.auto_scrape_attributes:
                 scrape_list.append('fake data')
-            self.scrapes_df.loc[len(self.scrapes_df.index) + 1] = scrape_list
+            self.auto_scrape_attribute_df.loc[len(self.auto_scrape_attribute_df.index) + 1] = scrape_list
 
     def join_dfs(self):
-        self.full_attribute_df = pd.concat([self.scrapes_df, self.manual_input_df], axis=1)
+        self.full_attribute_df = pd.concat([self.auto_scrape_attribute_df, self.manual_attribute_df], axis=1)
         print(self.full_attribute_df)
 
 if __name__ == '__main__':
-    es = DataInput(ATTRIBUTES, SCRAPES)
-    es.get_manual_input([1,2])
-    es.scrape_input([1,2])
+    es = DataInput(MANUAL_ATTRIBUTES, AUTO_SCRAPE_ATTRIBUTES)
+    es.get_manual_input([1, 2])
+    es.scrape_attributes([1, 2])
     es.join_dfs()
-    es.print_data()
+    print(es.manual_attribute_df)
