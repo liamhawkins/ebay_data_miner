06daa2a88e4c5b2be7eb0e536ba2c00197e2d0ad
Liam Hawkins <liamhawkinscu@gmail.com>
2017-04-25 18:28:33 -0400

    Remove test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from miner import EbayItem
-
-item = EbayItem({'id':'45324523', 'hdd':200})
-print(item.id)
-print(item.hdd)
-
-item.set_attributes({'id':'fdsadfas'}, {'temp':'adfasdf'})
-print(item.id)
-print(item.temp)
